017d1d34998bc8ea8fd7b1a1b5db9bff71c6b4a4
Charles Zhang <szinubuntu@gmail.com>
2024-02-19 20:24:23 -0500

    Initial setup

---
+++ LaTexRendering/Program.cs
@@ -0,0 +1,26 @@
+﻿using AngouriMath;
+using CSharpMath.SkiaSharp;
+
+namespace LaTexRendering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine()!;
+            string equation = ParseInput(input);
+
+            MathPainter painter = new MathPainter { LaTeX = equation }; // or TextPainter
+            using Stream? png = painter.DrawAsStream(format: SkiaSharp.SKEncodedImageFormat.Png);
+            using FileStream output = File.OpenWrite(args[0]);
+            png.CopyTo(output);
+        }
+
+        private static string ParseInput(string input)
+        {
+            // E.g. @"\frac23"
+            Entity expr = MathS.FromString(input); // This gives us a tree structure
+            return @"\frac23";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# LaTex Rendering
+
+A quicker parser setup (should have done in a single csproj or Pure) that tests parsing simple equation-like expressions into LaTex and performs a render.
