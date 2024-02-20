09c1765ec458fc5aaf3cb286338581987d8eaec2
Charles Zhang <szinubuntu@gmail.com>
2024-02-19 20:35:46 -0500

    Add analysis function

--- LaTexRendering/Program.cs
+++ LaTexRendering/Program.cs
@@ -7,20 +7,54 @@ namespace LaTexRendering
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine()!;
+            string input = @"sqrt(sum((V-u)^2)/n)";
             string equation = ParseInput(input);
 
-            MathPainter painter = new MathPainter { LaTeX = equation }; // or TextPainter
+            MathPainter painter = new() { LaTeX = equation }; // or TextPainter
             using Stream? png = painter.DrawAsStream(format: SkiaSharp.SKEncodedImageFormat.Png);
             using FileStream output = File.OpenWrite(args[0]);
             png.CopyTo(output);
         }
 
+        #region Routines
         private static string ParseInput(string input)
         {
-            // E.g. @"\frac23"
             Entity expr = MathS.FromString(input); // This gives us a tree structure
+            Report(expr); // Analysis us
+            return ConvertExpression(expr);
+        }
+
+        private static void Report(Entity expr)
+        {
+            var variables = GatherVariables(expr);
+            Console.WriteLine($"Variables: {string.Join(", ", variables)}");
+        }
+
+        private static string ConvertExpression(Entity expr)
+        {
+            // E.g. @"\frac23"
             return @"\frac23";
         }
+        #endregion
+
+        #region Equation Analysis
+        public static string[] GatherVariables(Entity rootEntity)
+        {
+            List<string> variables = [];
+            GatherFrom(variables, rootEntity);
+            return variables.Distinct().ToArray();
+
+            static void GatherFrom(List<string> container, Entity entity)
+            {
+                if (entity.Vars.Count() > 0)
+                    container.AddRange(entity.Vars.Select(v => v.Name));
+                if (entity.DirectChildren.Count > 0)
+                {
+                    foreach (var child in entity.DirectChildren)
+                        GatherFrom(container, child);
+                }
+            }
+        }
+        #endregion
     }
 }
